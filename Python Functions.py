06a0ebc8513b5d30c53975a2754bb0ccb7748c6c
SHIVAM KUMAR <151607127+shivam9142kumar@users.noreply.github.com>
2024-06-27 10:38:34 +0530

    06/06/2024 - Python Practice.

--- Python Functions.py
+++ Python Functions.py
@@ -67,7 +67,7 @@ def my_function(x):
   print(x)
 my_function(4)
 
-# Combine Positional-Only and Keyword-Only
+# Combine Positional-Only and Keyword-Only.
 def my_function(a, b, /, *, c, d):
   print(a + b + c + d)
 my_function(5, 6, c = 7, d = 8)
